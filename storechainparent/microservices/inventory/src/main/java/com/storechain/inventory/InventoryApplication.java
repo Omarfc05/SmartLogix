f7e5f073df7a3e90ffb9df07a976c535af2c8ac6
OmarFc05 <om.filun@duocuc.cl>
2026-05-19 17:20:20 -0400

    Se implementó Documentación Swagger a los 3 microservicios principales

--- storechainparent/microservices/inventory/src/main/java/com/storechain/inventory/InventoryApplication.java
+++ storechainparent/microservices/inventory/src/main/java/com/storechain/inventory/InventoryApplication.java
@@ -1,9 +1,18 @@
 package com.storechain.inventory;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API de Inventario - SmartLogix",
+				version = "1.0",
+				description = "Documentación del microservicio encargado de gestionar productos y control de stock."
+		)
+)
 public class InventoryApplication {
 
 	public static void main(String[] args) {
