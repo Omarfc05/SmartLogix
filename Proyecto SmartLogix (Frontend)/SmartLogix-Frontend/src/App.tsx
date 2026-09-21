5a242d0baeffea53f92d7ce1c555cf5435ab51dd
PAANBB05DST0105 <PAANBB05DST0105@PAANBB05DST0105.duoc.acad>
2026-09-21 10:44:02 -0300

    dockerizar

---
+++ Proyecto SmartLogix (Frontend)/SmartLogix-Frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+import { Cart } from "./pages/Cart";
+import { ProcederAlPago } from "./pages/ProcederAlPago";
+import { Navbar } from "./components/Navbar";
+import { Admin } from "./pages/Admin";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<ProcederAlPago />} />
+                <Route path="/admin" element={<Admin />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
